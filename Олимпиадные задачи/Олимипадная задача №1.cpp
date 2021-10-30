2c8327f2fa38a43dfd479559d302a7b7f0897643
Guseva Anastasia <92533196+GusevaAnastasia@users.noreply.github.com>
2021-10-30 19:28:01 +0500

    Add files via upload

---
+++ Олимпиадные задачи/Олимипадная задача №1.cpp
@@ -0,0 +1,82 @@
+﻿#include<iostream>
+using namespace std;
+
+
+ int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int X;
+	cout << "Введите трехзначное число, в котором нет повторяющихся цифр\n";
+	cin >> X;
+
+	while ((X < 100) || (X > 999))
+	{
+		cout << "Введенное число не трехзначное, повторите ввод\n";
+		cin >> X;
+	}
+
+	int a, b, c;
+	a = X % 10;
+	X = X/10;
+	b = X % 10;
+	c = X/10;
+	
+	while ((a == b) || (b == c) || (a == c))
+	{
+		cout << "В числе есть повторяющиеся цифры, повторите ввод\n";
+		cin >> X;
+		a = X % 10;
+		X = X / 10;
+		b = X % 10;
+		c = X / 10;
+	}
+	
+	cout << "Все возможные последовательности чисел:\n";
+	cout << a << b << c<<"\n";
+	cout << a << c << b<<"\n";
+	cout << b << a << c<<"\n";
+	cout << b << c << a<<"\n";
+	cout << c << a << b<<"\n";
+	cout << c << b << a<<"\n";
+
+	if ((a > b) && (a > c))
+	{
+		cout << "Наибольшее из этих чисел: " << a;
+		if (b > c)
+		{
+			cout << b << c;
+		}
+		else
+		{
+			cout << c << b;
+		}
+	}
+	else
+	{
+		if ((b > a) && (b > c))
+		{
+			cout << "Наибольшее из этих чисел: " << b;
+			if (a > c)
+			{
+				cout << a << c;
+			}
+			else
+			{
+				cout << c << a;
+			}
+		}
+		else
+		{
+			cout << "Наибольшее из этих чисел: " << c;
+			if (a > b)
+			{
+				cout << a << b;
+			}
+			else
+			{
+				cout << b << a;
+			}
+		}
+	}
+	return 0;
+}
---
+++ Олимпиадные задачи/Олимпиадная задача №2.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	srand(time(NULL));
+	setlocale(LC_ALL, "Russian");
+	int N;
+	N = rand() % 100;
+	int i = 0;
+	int X = 0;
+
+	cout << "Компьютер загадал число. Попробуйте его отгадать\n";
+
+	while ((i < 7)&&(X != N))
+	{	
+		cout << "Введите число от 0 до 100\n";
+		cin >> X;
+		i += 1;
+		if (X > N)
+		{
+			cout << "Ваше число больше загаданного\n";
+		}
+		else if (X < N)
+		{
+			cout << "Ваше число меньше загаданного\n";
+		}
+		else 
+		{
+			cout << "Поздраляем! Вы угадали число!\n";
+		}
+	}
+
+	if (i == 7)
+	{
+		cout << "К сожалению, вы не угадали число(\n";
+		cout << "Загаданное число: " << N << "\n";
+	}
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
